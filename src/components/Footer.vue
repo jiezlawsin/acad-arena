aec7b01577b6f9da4bcddba5ce3f3836a0a32a8a
donaroxasct <donalyn.roxas@codeandtheory.com>
2024-02-21 19:33:10 +0800

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,32 +1,58 @@
 <template>
   <section id="footer" class="py-5">
     <div class="container">
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+      <div class="sponsors">
+        <a href="#">
+          <img src="../assets/images/globe.png" alt="">
+        </a>
+        <a href="#">
+          <img src="../assets/images/riot.png" alt="">
+        </a>
+        <a href="#">
+          <img src="../assets/images/garena.png" alt="">
+        </a>
+        <a href="#">
+          <img src="../assets/images/moonton.png" alt="">
+        </a>
+        <a href="#">
+          <img src="../assets/images/bren-esports.png" alt="">
+        </a>
+        <a href="#">
+          <img src="../assets/images/pearson.png" alt="">
+        </a>
+        <a href="#">
+          <img src="../assets/images/rumble-royale.png" alt="">
+        </a>
       </div>
-      <div class="row sitemap">
-        <div class="col-sm-6">
+      <hr>
+      <div class="sitemap">
           <ul>
-            <li><b>COMPANY</b></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Our Team</a></li>
             <li><a href="#">Careers</a></li>
             <li><a href="#">Contact Us</a></li>
-            <li><a href="#">Support</a></li>
+            <li><a href="#">Suppor  t</a></li>
           </ul>
-        </div>
-        <div class="col-sm-6">
           <ul>
-            <li><b>LEGAL</b></li>
             <li><a href="#">Terms of Use</a></li>
             <li><a href="#">Privacy Policy</a></li>
             <li><a href="#">Code of Conduct</a></li>
           </ul>
-        </div>
+          <ul>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Code of Conduct</a></li>
+          </ul>
+          <ul>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Code of Conduct</a></li>
+          </ul>
+      </div>
+      <hr>
+      <div class="copyright">
+        <div class="logo"><img src="../assets/images/logo-colored.png" alt=""></div>
+        <div class="copy">&copy; 2022 AcadArena &bull; AcadArena is a trademarked brand owned by AcadArena Technologies Inc. - A Singapore-registered company. All other trademarks are owned by their respective owners.</div>
       </div>
     </div>
   </section>
