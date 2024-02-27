d582f7c08af65fc912c3f4acc8c0a74bcddc590c
donaroxasct <donalyn.roxas@codeandtheory.com>
2024-02-27 18:28:51 +0800

    Buffs view page

--- src/mock/buffs.ts
+++ src/mock/buffs.ts
@@ -1,10 +1,101 @@
-export default [
+export const BuffsList = [
   {
     id: 'unipin-mlbb-tournament-buff',
     name: 'UniPin MLBB Tournament Buff',
+    description: 'Lorem ipsum—dolor sit amet, consectetur adipiscing elit.',
+    heroImage: 'https://pinoygamer.ph/attachments/image-000-png.9257/',
+    sponsors: [
+      {
+        name: 'Riot Games',
+        logo: 'https://i.pinimg.com/originals/90/6d/23/906d231cbaff55c77fb97191592e1c76.png',
+      },
+    ],
+    eligibleEvents: [
+      {
+        name: 'Offline Tourney.',
+        description: 'No pressure.',
+      },
+      {
+        name: '16+ Teams.',
+        description: 'Minimum participants.',
+      },
+      {
+        name: 'Student-only',
+        description: 'Eigibiity.',
+      },
+      {
+        name: 'Broadcasted.',
+        description: 'Complete with casters.',
+      },
+      {
+        name: '1-day Event.',
+        description: 'Short and sweet.',
+      },
+    ],
+    ctaBreaker: {
+      smallText: 'This buff gives',
+      bigText: '3000 UC',
+      description: '<p class="text-gradient ">UniPin Credits will be inserted directly to their accounts.</p><p>Subscribe to our newsletter or join our Discord Server.</p>',
+      list: [],
+      image: 'https://cdn.vectorstock.com/i/preview-1x/78/97/online-education-concept-vector-38217897.webp',
+      cta: {
+        ctaText: 'Apply Now',
+        ctaLink: '#',
+      },
+    }
   },
   {
     id: 'dewday-tournament-buff',
     name: '#DewDay Tournament Buff',
+    description: 'Lorem ipsum—dolor sit amet, consectetur adipiscing elit.',
+    heroImage: 'https://pinoygamer.ph/attachments/image-000-png.9257/',
+    sponsors: [
+      {
+        name: 'Bren Esports',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/d/df/Bren_Esports.png',
+      },
+    ],
+    eligibleEvents: [
+      {
+        name: 'Offline Tourney.',
+        description: 'No pressure.',
+      },
+      {
+        name: '16+ Teams.',
+        description: 'Minimum participants.',
+      },
+      {
+        name: 'Student-only.',
+        description: 'Eigibiity.',
+      },
+      {
+        name: 'Broadcasted.',
+        description: 'Complete with casters.',
+      },
+      {
+        name: '1-day Event.',
+        description: 'Short and sweet.',
+      },
+      {
+        name: 'Platform.',
+        description: 'Host signs up in AcadArena.',
+      },
+    ],
+    ctaBreaker: {
+      smallText: 'This buff gives',
+      bigText: '',
+      description: '',
+      image: 'https://cdn.vectorstock.com/i/preview-1x/78/97/online-education-concept-vector-38217897.webp',
+      list: [
+        '1000 tons of Mountain Dew',
+        '5 pcs. Mountain Dew Shirts',
+        '1px. Mountain Dew Roll-up Banner',
+        'Happiness',
+      ],
+      cta: {
+        ctaText: 'Apply Now',
+        ctaLink: '#',
+      },
+    }
   }
 ]
--- src/views/BuffView.vue
+++ src/views/BuffView.vue
@@ -1,25 +1,67 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Banner from '../components/Banner.vue'
-import BuffsList from '../mock/buffs.ts'
+import { BuffsList } from '../mock/buffs'
 
 export default defineComponent({
   name: 'buffView',
   title: 'BuffView',
   data() {
     return {
-      data: null
+      data: BuffsList.find(buff => buff.id === this.$route.params.id)
     };
   },
   mounted() {
-    console.log(BuffsList)
+    const eventCount = this.data?.eligibleEvents.length ?? 0;
+    if(eventCount < 6) {
+      const diff = 6 - eventCount;
+      for(let i = 0; i < diff; i++) {
+        this.data?.eligibleEvents.push({});
+      }
+    }
   }
 })
 </script>
 
 
 <template>
-  <main>
-    buff view
+  <main class="buffs-view-page">
+    <div class="container">
+      <div class="hero-banner">
+        <img :src="data?.heroImage" alt="">
+      </div>
+      <h1>{{ data?.name }}</h1>
+      <div class="sponsors" v-if="data?.sponsors">
+        <span>powered by</span>
+        <img v-for="sponsor in data?.sponsors" :src="sponsor.logo" :alt="sponsor.name">
+      </div>
+      <div class="events">
+        <div class="heading">Eligible Events</div>
+        <div class="cards">
+          <div class="event-card" :class="{'is-empty': !event.name}" v-for="event in data?.eligibleEvents">
+            <div class="event-name">{{ event.name }}</div>
+            <div class="event-description">{{ event.description }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="description">{{ data?.description }}</div>
+    </div>
+    <div class="cta-breaker">
+      <div class="container">
+        <div class="wrapper d-flex">
+          <div class="image w-50">
+            <img :src="data?.ctaBreaker.image" alt="">
+          </div>
+          <div class="content w-50">
+            <div class="small-text" v-if="data?.ctaBreaker.smallText">{{ data?.ctaBreaker.smallText }}</div>
+            <div class="big-text" v-if="data?.ctaBreaker.bigText">{{ data?.ctaBreaker.bigText }}</div>
+            <div class="description" v-if="data?.ctaBreaker.description" v-html="data?.ctaBreaker.description"></div>
+            <ul v-if="data?.ctaBreaker.list" class="list">
+              <li v-for="item in data?.ctaBreaker.list"><i class="bi bi-check-lg"></i> {{ item }}</li>
+            </ul>
+            <a :href="data?.ctaBreaker.cta.ctaLink" class="cta"><i class="bi bi-clipboard2-check-fill"></i> {{ data?.ctaBreaker.cta.ctaText }}</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
