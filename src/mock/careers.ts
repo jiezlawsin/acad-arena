e6fa9243d72fdb8279f7e40152346b616c9cb27c
donaroxasct <donalyn.roxas@codeandtheory.com>
2024-10-25 15:51:09 +0800

    Career view init

---
+++ src/mock/careers.ts
@@ -0,0 +1,32 @@
+export const CareersList = [
+  {
+    id: 'branding-workshop',
+    name: 'Branding Workshop',
+    tag: 'with overdrive studios',
+    category: 'classes',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    body: '',
+    cardImage: '/images/program2.jpg',
+    images: [],
+    classSchedule: '',
+    applicationSchedule: '',
+    cardIcons: [
+      {
+        name: 'Nov-Jan',
+        icon: '/images/icons/calendar.png'
+      },
+      {
+        name: '40-200+',
+        icon: '/images/icons/people.png'
+      },
+      {
+        name: 'College',
+        icon: '/images/icons/confetti.png'
+      },
+      {
+        name: 'Design Major',
+        icon: '/images/icons/confetti.png'
+      },
+    ],
+  },
+]
---
+++ src/views/CareerView.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { CareersList } from '../mock/careers'
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  name: 'careerView',
+  title: 'CareerView',
+  data() {
+    return {
+      data: CareersList.find(career => career.id === this.$route.params.id)
+    };
+  },
+  mounted() {
+    const router = useRouter();
+    if (!this.data) {
+      router.push('/notfound');
+    }
+  }
+})
+</script>
+
+
+<template>
+  <main class="careers-view-page">
+    <div class="container career-view-container">
+      <div class="content">
+        <h1>{{ data?.name }}</h1>
+        <div class="tag">{{ data?.tag }}</div>
+        <hr>
+      </div>
+      <div class="sticky-card">
+        <div class="card-body"></div>
+        <a href="" class="card-cta btn">Apply Now</a>
+      </div>
+    </div>
+  </main>
+</template>
