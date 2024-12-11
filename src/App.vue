1b7ded55276f6f222be1d08337963c175fc7f367
Aries Jiez <Lawsin>
2024-12-11 19:29:35 +0800

    Revert "Time Bomb"
    This reverts commit 770a1560e9bbe7d48b0ba64fd6c2b4463c06ba91.

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,10 @@ import { defineComponent } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
-import NotFound  from './views/404.vue';
 </script>
 
 <template>
-  <NotFound />
-  <!-- <Navbar/>
+  <Navbar/>
   <RouterView />
-  <Footer/> -->
+  <Footer/>
 </template>
